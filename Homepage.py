cce1ba595e06c3dcb32adf98bdb81ef635d75dc4
RaymondSatoshi <mincong.cmc@gmail.com>
2023-10-04 23:09:08 +0800

    1004

--- Homepage.py
+++ Homepage.py
@@ -1,21 +1,38 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "print(\"xY\")\n"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import config
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import(
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
+config.init_session()
+
+
+if st.session_state["OPENAI_API_KEY"]:
+    chat = ChatOpenAI(openai_api_key=st.session_state["OPENAI_API_KEY"])
+else:
+    chat = None
+
+st.set_page_config(page_title="Lakehouse-ai", layout="wide")
+st.title("TalkWithLakehouse")
+
+# with st.container():
+#     st.header("Config")
+#     st.markdown(f"""
+#         |OpenAI API Key|PINECONE API KEY|PINECONE ENVIRONMENT|
+#         |-|-|-|
+#         |{st.session_state["OPENAI_API_KEY"]}|{st.session_state["PINECONE_API_KEY"]}|{st.session_state["PINECONE_ENVIRONMENT"]}|
+#      """)
+    
+with st.container():
+    # st.header("Chat With Lakehouse")
+    if chat:
+        question = st.text_input("Question", value="", max_chars=None, key=None, type="default")
+        ask = st.button("Ask")
+        if ask:
+            response = chat([HumanMessage(content=question)])
+            st.write(response.content)
+    else:
+        st.warning("plz set your key in the config page")
---
+++ pages/1_🛠_Config.py
@@ -0,0 +1,19 @@
+import config
+import streamlit as st
+
+config.init_session()
+
+st.set_page_config(page_title="Config", layout="wide")
+
+st.title("Config")
+
+openai_api_key = st.text_input("OPENAI_API_KEY", value=st.session_state["OPENAI_API_KEY"], max_chars=None, key=None, type="password")
+pinecone_api_key = st.text_input("PINECONE_API_KEY", value=st.session_state["PINECONE_API_KEY"], max_chars=None, key=None, type="password")
+pinecone_environment = st.text_input("PINECONE_ENVIRONMENT", value=st.session_state["PINECONE_ENVIRONMENT"], max_chars=None, key=None, type="password")
+
+saved = st.button("save")
+
+if saved:
+    st.session_state["OPENAI_API_KEY"] = openai_api_key
+    st.session_state["PINECONE_API_KEY"] = pinecone_api_key
+    st.session_state["PINECONE_ENVIRONMENT"] = pinecone_environment
