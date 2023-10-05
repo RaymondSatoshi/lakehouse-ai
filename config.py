c0918dad516c696f628b8c25aff3eac5cdf42c6e
RaymondSatoshi <mincong.cmc@gmail.com>
2023-10-05 09:31:44 +0800

    100502

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def init_session():
-    st.session_state.setdefault("OPENAI_API_KEY", "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")
+    st.session_state.setdefault("OPENAI_API_KEY", "")
     st.session_state.setdefault("PINECONE_API_KEY", "")
     st.session_state.setdefault("PINECONE_ENVIRONMENT", "")
     st.session_state.setdefault("MESSAGE", [])
