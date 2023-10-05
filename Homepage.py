23a76b9bd0ecb18dfd8c25fc5f65f7726eb641e3
RaymondSatoshi <mincong.cmc@gmail.com>
2023-10-05 09:30:05 +0800

    10050930

--- Homepage.py
+++ Homepage.py
@@ -9,7 +9,6 @@ from langchain.schema import(
 
 config.init_session()
 
-
 if st.session_state["OPENAI_API_KEY"]:
     chat = ChatOpenAI(openai_api_key=st.session_state["OPENAI_API_KEY"])
 else:
@@ -18,21 +17,41 @@ else:
 st.set_page_config(page_title="Lakehouse-ai", layout="wide")
 st.title("TalkWithLakehouse")
 
-# with st.container():
-#     st.header("Config")
-#     st.markdown(f"""
-#         |OpenAI API Key|PINECONE API KEY|PINECONE ENVIRONMENT|
-#         |-|-|-|
-#         |{st.session_state["OPENAI_API_KEY"]}|{st.session_state["PINECONE_API_KEY"]}|{st.session_state["PINECONE_ENVIRONMENT"]}|
-#      """)
-    
+for message in st.session_state["MESSAGE"]:
+    if isinstance(message, HumanMessage):
+        with st.chat_message("user"):
+            st.markdown(message.content)
+    elif isinstance(message, AIMessage):
+        with st.chat_message("assistant"):
+            st.markdown(message.content)
+
+if not chat: st.warning("plz set your key in the config page") 
+
 with st.container():
-    # st.header("Chat With Lakehouse")
-    if chat:
-        question = st.text_input("Question", value="", max_chars=None, key=None, type="default")
-        ask = st.button("Ask")
-        if ask:
-            response = chat([HumanMessage(content=question)])
-            st.write(response.content)
-    else:
-        st.warning("plz set your key in the config page")
+    question = st.chat_input("Type Something...")
+    # question = st.text_input("Question", value="", max_chars=None, key=None, type="default")
+    # ask = st.button("Ask")
+    if question:
+        st.session_state["MESSAGE"].append(HumanMessage(content=question))
+        response = chat([HumanMessage(content=question)])
+        st.session_state["MESSAGE"].append(response)
+
+    for message in st.session_state["MESSAGE"]:
+        if isinstance(message, AIMessage):
+            with st.chat_message("assistant"):
+                st.markdown(message.content)
+        else:
+            with st.chat_message("user"):
+                st.markdown(message.content)
+
+### sidebar
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
--- config.py
+++ config.py
@@ -1,15 +1,7 @@
 import streamlit as st
 
 def init_session():
-    st.session_state.setdefault("OPENAI_API_KEY", "")
+    st.session_state.setdefault("OPENAI_API_KEY", "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")
     st.session_state.setdefault("PINECONE_API_KEY", "")
     st.session_state.setdefault("PINECONE_ENVIRONMENT", "")
-    # if "OPENAI_API_KEY" not in st.session_state:
-    #     st.session_state["OPENAI_API_KEY"] = ""
-    # else:
-    #     if not st.session_state["OPENAI_API_KEY"] == "" :
-    #         chat = ChatOpenAI(open_api_key=st.session_state["OPENAI_API_KEY"])
-    # if "PINECONE_API_KEY" not in st.session_state:
-    #     st.session_state["PINECONE_API_KEY"] = ""
-    # if "PINECONE_ENVIRONMENT" not in st.session_state:
-    #     st.session_state["PINECONE_ENVIRONMENT"] = "" 
+    st.session_state.setdefault("MESSAGE", [])
