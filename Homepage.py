7a20c3e77714a1cf17eea4e88c8b648650c795ae
RaymondSatoshi <mincong.cmc@gmail.com>
2023-10-05 09:39:41 +0800

    fix the api_key problem

--- Homepage.py
+++ Homepage.py
@@ -43,15 +43,3 @@ with st.container():
         else:
             with st.chat_message("user"):
                 st.markdown(message.content)
-
-### sidebar
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
-
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
