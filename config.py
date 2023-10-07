9d3ed384e82b9144dae2f4694951365f4e480028
RaymondSatoshi <mincong.cmc@gmail.com>
2023-10-07 08:01:20 +0800

    change

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
+import os
 import streamlit as st
+from dotenv import find_dotenv, load_dotenv
+_ = load_dotenv(find_dotenv())
+
+if os.environ["OPENAI_API_KEY"]:
+     st.session_state["OPENAI_API_KEY"] = os.environ["OPENAI_API_KEY"]
 
 def init():
     st.set_page_config(page_title="Lakehouse-ai", layout="wide")
-    st.session_state.setdefault("OPENAI_API_KEY", "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")
+    st.session_state.setdefault("OPENAI_API_KEY", "")
     st.session_state.setdefault("PINECONE_API_KEY", "")
     st.session_state.setdefault("PINECONE_ENVIRONMENT", "")
     st.session_state.setdefault("MESSAGE", [])
